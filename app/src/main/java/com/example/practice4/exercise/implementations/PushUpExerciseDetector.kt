2395291baafbae276cbc00536e572acc2b66a118
Александр <pic@MacStudio-Aleksandr.local>
2026-04-08 23:38:03 +0300

    Fix missing Singleton import in PushUpExerciseDetector

--- app/src/main/java/com/example/practice4/exercise/implementations/PushUpExerciseDetector.kt
+++ app/src/main/java/com/example/practice4/exercise/implementations/PushUpExerciseDetector.kt
@@ -7,6 +7,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
+import javax.inject.Singleton
 
 @Singleton
 class PushUpExerciseDetector @Inject constructor() : IExerciseDetector {
