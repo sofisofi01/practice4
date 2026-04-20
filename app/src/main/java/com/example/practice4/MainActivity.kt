81ff3e8bf17d636aab15d411fcd96186795e6035
Александр <pic@MacStudio-Aleksandr.local>
2026-04-20 14:17:20 +0300

    Fix experimental Material 3 API warning

--- app/src/main/java/com/example/practice4/MainActivity.kt
+++ app/src/main/java/com/example/practice4/MainActivity.kt
@@ -56,6 +56,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
     var selectedTab by remember { mutableIntStateOf(0) }
