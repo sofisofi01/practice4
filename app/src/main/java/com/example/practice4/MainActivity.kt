acc9fec97087f0920c469165d3dc8f9d362435d4
Александр <pic@MacStudio-Aleksandr.local>
2026-04-20 14:29:06 +0300

    Fix missing Build import in MainActivity

--- app/src/main/java/com/example/practice4/MainActivity.kt
+++ app/src/main/java/com/example/practice4/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.practice4
 
 import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
